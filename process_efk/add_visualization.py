cde57885b3c1fd2a45829f02ad1e813d3fa4e7df
yeicandoit <410342333@qq.com>
2018-04-23 17:34:37 +0800

    Enable to add visualization from file

--- process_efk/add_visualization.py
+++ process_efk/add_visualization.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
 import requests
 import json
 import copy
+import string
 
 
 URL_ELASTICSEARCH_KIBANA = "http://localhost:9200/.kibana/doc/visualization:%s"
@@ -36,48 +36,68 @@ def get_keys():
         return []
 
 
-def add_pv(keys=[]):
-    data = {"type": "visualization", "visualization": {"title": "app_video_detail_bottom_act_show PV", "visState": "{\"title\":\"app_video_detail_bottom_act_show PV\",\"type\":\"histogram\",\"params\":{\"type\":\"histogram\",\"grid\":{\"categoryLines\":false,\"style\":{\"color\":\"#eee\"}},\"categoryAxes\":[{\"id\":\"CategoryAxis-1\",\"type\":\"category\",\"position\":\"bottom\",\"show\":true,\"style\":{},\"scale\":{\"type\":\"linear\"},\"labels\":{\"show\":true,\"truncate\":100},\"title\":{}}],\"valueAxes\":[{\"id\":\"ValueAxis-1\",\"name\":\"LeftAxis-1\",\"type\":\"value\",\"position\":\"left\",\"show\":true,\"style\":{},\"scale\":{\"type\":\"linear\",\"mode\":\"normal\"},\"labels\":{\"show\":true,\"rotate\":0,\"filter\":false,\"truncate\":100},\"title\":{\"text\":\"pv\"}}],\"seriesParams\":[{\"show\":\"true\",\"type\":\"histogram\",\"mode\":\"stacked\",\"data\":{\"label\":\"pv\",\"id\":\"1\"},\"valueAxis\":\"ValueAxis-1\",\"drawLinesBetweenPoints\":true,\"showCircles\":true}],\"addTooltip\":true,\"addLegend\":true,\"legendPosition\":\"right\",\"times\":[],\"addTimeMarker\":false},\"aggs\":[{\"id\":\"1\",\"enabled\":true,\"type\":\"count\",\"schema\":\"metric\",\"params\":{\"customLabel\":\"pv\"}},{\"id\":\"2\",\"enabled\":true,\"type\":\"date_histogram\",\"schema\":\"segment\",\"params\":{\"field\":\"@timestamp\",\"interval\":\"d\",\"customInterval\":\"2h\",\"min_doc_count\":1,\"extended_bounds\":{}}}]}", "uiStateJSON": "{}", "description": "", "version": 1, "kibanaSavedObjectMeta": {
+def add_pv():
+    data = {"type": "visualization", "visualization": {"title": "app_video_detail_bottom_act_show PV", "visState": "{\"title\":\"app_video_detail_bottom_act_show\",\"type\":\"histogram\",\"params\":{\"type\":\"histogram\",\"grid\":{\"categoryLines\":false,\"style\":{\"color\":\"#eee\"}},\"categoryAxes\":[{\"id\":\"CategoryAxis-1\",\"type\":\"category\",\"position\":\"bottom\",\"show\":true,\"style\":{},\"scale\":{\"type\":\"linear\"},\"labels\":{\"show\":true,\"truncate\":100},\"title\":{}}],\"valueAxes\":[{\"id\":\"ValueAxis-1\",\"name\":\"LeftAxis-1\",\"type\":\"value\",\"position\":\"left\",\"show\":true,\"style\":{},\"scale\":{\"type\":\"linear\",\"mode\":\"normal\"},\"labels\":{\"show\":true,\"rotate\":0,\"filter\":false,\"truncate\":100},\"title\":{\"text\":\"pv\"}}],\"seriesParams\":[{\"show\":\"true\",\"type\":\"histogram\",\"mode\":\"stacked\",\"data\":{\"label\":\"pv\",\"id\":\"1\"},\"valueAxis\":\"ValueAxis-1\",\"drawLinesBetweenPoints\":true,\"showCircles\":true}],\"addTooltip\":true,\"addLegend\":true,\"legendPosition\":\"right\",\"times\":[],\"addTimeMarker\":false},\"aggs\":[{\"id\":\"1\",\"enabled\":true,\"type\":\"count\",\"schema\":\"metric\",\"params\":{\"customLabel\":\"pv\"}},{\"id\":\"2\",\"enabled\":true,\"type\":\"date_histogram\",\"schema\":\"segment\",\"params\":{\"field\":\"@timestamp\",\"interval\":\"d\",\"customInterval\":\"2h\",\"min_doc_count\":1,\"extended_bounds\":{}}}]}", "uiStateJSON": "{}", "description": "", "version": 1, "kibanaSavedObjectMeta": {
         "searchSourceJSON": "{\"index\":\"5a839130-43ba-11e8-9740-7b993571f659\",\"filter\":[{\"meta\":{\"index\":\"5a839130-43ba-11e8-9740-7b993571f659\",\"negate\":false,\"disabled\":false,\"alias\":null,\"type\":\"phrase\",\"key\":\"key.keyword\",\"value\":\"app_video_detail_bottom_act_show\",\"params\":{\"query\":\"app_video_detail_bottom_act_show\",\"type\":\"phrase\"}},\"query\":{\"match\":{\"key.keyword\":{\"query\":\"app_video_detail_bottom_act_show\",\"type\":\"phrase\"}}},\"$state\":{\"store\":\"appState\"}}],\"query\":{\"query\":\"\",\"language\":\"lucene\"}}"}}}
 
-    for key in keys:
-        url = URL_ELASTICSEARCH_KIBANA % key + "_PV"
-        post_data = copy.deepcopy(data)
-        post_data["visualization"]["title"] = key + " PV"
-        post_data["visualization"]["visState"] = post_data["visualization"]["visState"].replace(
-            "app_video_detail_bottom_act_show", key)
-        post_data["visualization"]["kibanaSavedObjectMeta"]["searchSourceJSON"] = post_data["visualization"]["kibanaSavedObjectMeta"]["searchSourceJSON"].replace(
-            "app_video_detail_bottom_act_show", key)
-        # print post_data
-        requests.post(url, headers=JSON_HEADER,
-                      data=json.dumps(post_data), timeout=(10, 20))
+    with open('data_statistics.csv', 'r') as f:
+        for line in f.readlines():
+            info = line.strip().split(',')
+            key = info[1]
+            if string.find(key, "pv") == -1:
+		continue 
+            raw_key = info[2]
+            name = info[3].decode("GBK")
+            url = URL_ELASTICSEARCH_KIBANA % raw_key + "_PV"
+            post_data = copy.deepcopy(data)
+            post_data["visualization"]["title"] = name + \
+                "(" + raw_key + ")"
+            post_data["visualization"]["visState"] = post_data["visualization"]["visState"].replace(
+                "app_video_detail_bottom_act_show", name + "(" + raw_key + ")")
+            post_data["visualization"]["kibanaSavedObjectMeta"]["searchSourceJSON"] = post_data["visualization"]["kibanaSavedObjectMeta"]["searchSourceJSON"].replace(
+                "app_video_detail_bottom_act_show", raw_key)
+            # print post_data
+            requests.post(url, headers=JSON_HEADER,
+                          data=json.dumps(post_data), timeout=(10, 20))
 
 
 def add_uv(keys=[]):
-    data = {"type": "visualization", "visualization": {"title": "app_video_detail_bottom_act_show UV", "visState": "{\"title\":\"app_video_detail_bottom_act_show UV\",\"type\":\"histogram\",\"params\":{\"type\":\"histogram\",\"grid\":{\"categoryLines\":false,\"style\":{\"color\":\"#eee\"}},\"categoryAxes\":[{\"id\":\"CategoryAxis-1\",\"type\":\"category\",\"position\":\"bottom\",\"show\":true,\"style\":{},\"scale\":{\"type\":\"linear\"},\"labels\":{\"show\":true,\"truncate\":100},\"title\":{}}],\"valueAxes\":[{\"id\":\"ValueAxis-1\",\"name\":\"LeftAxis-1\",\"type\":\"value\",\"position\":\"left\",\"show\":true,\"style\":{},\"scale\":{\"type\":\"linear\",\"mode\":\"normal\"},\"labels\":{\"show\":true,\"rotate\":0,\"filter\":false,\"truncate\":100},\"title\":{\"text\":\"uv\"}}],\"seriesParams\":[{\"show\":\"true\",\"type\":\"histogram\",\"mode\":\"stacked\",\"data\":{\"label\":\"uv\",\"id\":\"1\"},\"valueAxis\":\"ValueAxis-1\",\"drawLinesBetweenPoints\":true,\"showCircles\":true}],\"addTooltip\":true,\"addLegend\":true,\"legendPosition\":\"right\",\"times\":[],\"addTimeMarker\":false},\"aggs\":[{\"id\":\"1\",\"enabled\":true,\"type\":\"cardinality\",\"schema\":\"metric\",\"params\":{\"field\":\"user_id.keyword\",\"customLabel\":\"uv\"}},{\"id\":\"2\",\"enabled\":true,\"type\":\"date_histogram\",\"schema\":\"segment\",\"params\":{\"field\":\"@timestamp\",\"interval\":\"d\",\"customInterval\":\"2h\",\"min_doc_count\":1,\"extended_bounds\":{}}}]}", "uiStateJSON": "{}", "description": "", "version": 1, "kibanaSavedObjectMeta": {
+    data = {"type": "visualization", "visualization": {"title": "app_video_detail_bottom_act_show UV", "visState": "{\"title\":\"app_video_detail_bottom_act_show\",\"type\":\"histogram\",\"params\":{\"type\":\"histogram\",\"grid\":{\"categoryLines\":false,\"style\":{\"color\":\"#eee\"}},\"categoryAxes\":[{\"id\":\"CategoryAxis-1\",\"type\":\"category\",\"position\":\"bottom\",\"show\":true,\"style\":{},\"scale\":{\"type\":\"linear\"},\"labels\":{\"show\":true,\"truncate\":100},\"title\":{}}],\"valueAxes\":[{\"id\":\"ValueAxis-1\",\"name\":\"LeftAxis-1\",\"type\":\"value\",\"position\":\"left\",\"show\":true,\"style\":{},\"scale\":{\"type\":\"linear\",\"mode\":\"normal\"},\"labels\":{\"show\":true,\"rotate\":0,\"filter\":false,\"truncate\":100},\"title\":{\"text\":\"uv\"}}],\"seriesParams\":[{\"show\":\"true\",\"type\":\"histogram\",\"mode\":\"stacked\",\"data\":{\"label\":\"uv\",\"id\":\"1\"},\"valueAxis\":\"ValueAxis-1\",\"drawLinesBetweenPoints\":true,\"showCircles\":true}],\"addTooltip\":true,\"addLegend\":true,\"legendPosition\":\"right\",\"times\":[],\"addTimeMarker\":false},\"aggs\":[{\"id\":\"1\",\"enabled\":true,\"type\":\"cardinality\",\"schema\":\"metric\",\"params\":{\"field\":\"user_id.keyword\",\"customLabel\":\"uv\"}},{\"id\":\"2\",\"enabled\":true,\"type\":\"date_histogram\",\"schema\":\"segment\",\"params\":{\"field\":\"@timestamp\",\"interval\":\"d\",\"customInterval\":\"2h\",\"min_doc_count\":1,\"extended_bounds\":{}}}]}", "uiStateJSON": "{}", "description": "", "version": 1, "kibanaSavedObjectMeta": {
         "searchSourceJSON": "{\"index\":\"5a839130-43ba-11e8-9740-7b993571f659\",\"filter\":[{\"meta\":{\"index\":\"5a839130-43ba-11e8-9740-7b993571f659\",\"negate\":false,\"disabled\":false,\"alias\":null,\"type\":\"phrase\",\"key\":\"key.keyword\",\"value\":\"app_video_detail_bottom_act_show\",\"params\":{\"query\":\"app_video_detail_bottom_act_show\",\"type\":\"phrase\"}},\"query\":{\"match\":{\"key.keyword\":{\"query\":\"app_video_detail_bottom_act_show\",\"type\":\"phrase\"}}},\"$state\":{\"store\":\"appState\"}},{\"meta\":{\"index\":\"5a839130-43ba-11e8-9740-7b993571f659\",\"negate\":true,\"disabled\":false,\"alias\":null,\"type\":\"phrase\",\"key\":\"user_id\",\"value\":\"-1\",\"params\":{\"query\":\"-1\",\"type\":\"phrase\"}},\"query\":{\"match\":{\"user_id\":{\"query\":\"-1\",\"type\":\"phrase\"}}},\"$state\":{\"store\":\"appState\"}}],\"query\":{\"query\":\"\",\"language\":\"lucene\"}}"}}}
-    for key in keys:
-        url = URL_ELASTICSEARCH_KIBANA % key + "_UV"
-        post_data = copy.deepcopy(data)
-        post_data["visualization"]["title"] = key + " UV"
-        post_data["visualization"]["visState"] = post_data["visualization"]["visState"].replace(
-            "app_video_detail_bottom_act_show", key)
-        post_data["visualization"]["kibanaSavedObjectMeta"]["searchSourceJSON"] = post_data["visualization"]["kibanaSavedObjectMeta"]["searchSourceJSON"].replace(
-            "app_video_detail_bottom_act_show", key)
-        # print post_data
-        requests.post(url, headers=JSON_HEADER,
-                      data=json.dumps(post_data), timeout=(10, 20))
+    with open('data_statistics.csv', 'r') as f:
+        for line in f.readlines():
+            info = line.strip().split(',')
+            key = info[1]
+            if string.find(key, "uv") == -1:
+		continue
+            raw_key = info[2]
+            name = info[3].decode("GBK")
+            url = URL_ELASTICSEARCH_KIBANA % raw_key + "_UV"
+            post_data = copy.deepcopy(data)
+            post_data["visualization"]["title"] = name + \
+                "(" + raw_key + ")"
+            post_data["visualization"]["visState"] = post_data["visualization"]["visState"].replace(
+                "app_video_detail_bottom_act_show", name + "(" + raw_key + ")")
+            post_data["visualization"]["kibanaSavedObjectMeta"]["searchSourceJSON"] = post_data["visualization"]["kibanaSavedObjectMeta"]["searchSourceJSON"].replace(
+                "app_video_detail_bottom_act_show", raw_key)
+            # print post_data
+            requests.post(url, headers=JSON_HEADER,
+                          data=json.dumps(post_data), timeout=(10, 20))
 
 
 def del_visualization(keys=[]):
-    for key in keys:
-	# TODO _id should consider pv and uv
-        url = URL_ELASTICSEARCH_KIBANA % key
-        requests.delete(url, timeout=(10, 20))
+    with open('data_statistics.csv', 'r') as f:
+        for line in f.readlines():
+            info = line.strip().split(',')
+            key = info[2]
+            url_pv = URL_ELASTICSEARCH_KIBANA % key + "_PV"
+            requests.delete(url_pv, timeout=(10, 20))
+            url_uv = URL_ELASTICSEARCH_KIBANA % key + "_UV"
+            requests.delete(url_uv, timeout=(10, 20))
 
 
 if __name__ == '__main__':
-    keys = get_keys()
-    # add_pv(keys)
-    add_uv(keys)
-    # del_visualization(keys)
+    add_pv()
+    add_uv()
+    # keys = get_keys()
+    # del_visualization()
