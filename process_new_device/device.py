e607763bd84e27e1498a44b8166e50b559156eeb
yeicandoit <410342333@qq.com>
2018-04-23 14:07:37 +0800

    realy remove process_new_device

--- process_new_device/device.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-if __name__ == '__main__':
-    with open('device.csv', 'r') as f:
-        for line in f.readlines():
-            device_id, first_time = line.strip().split(',')
-            timestamp = datetime.datetime.strptime(first_time,'%Y-%m-%d %H:%M:%S').isoformat()
-            print '{"device_id":"%s", "@timestamp":"%s"}' % (device_id,
-                                                             timestamp+"+08:00")
