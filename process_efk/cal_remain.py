7a4763720987f7561bc3efa9b97fa42785b54aa7
yeicandoit <410342333@qq.com>
2018-04-28 14:44:47 +0800

    cal remain rate

--- process_efk/cal_remain.py
+++ process_efk/cal_remain.py
@@ -10,7 +10,43 @@ from datetime import datetime, timedelta
 
 URL_ELASTICSEARCH_APPLOG = "http://localhost:9200/applog-*/doc/_search"
 URL_ELASTICSEARCH_DEVICE = "http://localhost:9200/device/doc/_search"
+URL_ELASTICSEARCH_REMAIN = "http://localhost:9200/remain"
+URL_ELASTICSEARCH_REMAIN_ID = "http://localhost:9200/remain/doc/%s"
 JSON_HEADER = {"Content-Type": "application/json"}
+REMAIN_MAPPING = {
+    "properties": {
+        "@timestamp": {
+            "type": "date"
+        },
+        "new_device_num": {
+            "type": "long"
+        },
+        "remain_rate_1d": {
+            "type": "float"
+        },
+        "remain_rate_2d": {
+            "type": "float"
+        },
+        "remain_rate_3d": {
+            "type": "float"
+        },
+        "remain_rate_4d": {
+            "type": "float"
+        },
+        "remain_rate_5d": {
+            "type": "float"
+        },
+        "remain_rate_7d": {
+            "type": "float"
+        },
+        "remain_rate_14d": {
+            "type": "float"
+        },
+        "remain_rate_30d": {
+            "type": "float"
+        },
+    }
+}
 
 
 def get_query_device(ndays):
@@ -21,8 +57,8 @@ def get_query_device(ndays):
         "size": 0,
         "aggs": {
             "uniq_device": {
-                "terms": { # get the device_id arry by terms, 
-                           # but the arry size may be smaller than unique device_id size got by cardinality
+                "terms": {  # get the device_id arry by terms,
+                    # but the arry size may be smaller than unique device_id size got by cardinality
                     "field": "device_id.keyword",
                     "size": 1000000000
                 }
@@ -72,17 +108,39 @@ def uniq_device(url="", query={}):
         return 1
 
 
-def update_remain(app_stay_first=1, app_stay=1):
-    app_stay_data = {
-        "@timestamp": datetime.today().isoformat() + "+08:00",
-        "app_stay": app_stay,
-        "app_stay_first": app_stay_first
-    }
-    requests.post(URL_ELASTICSEARCH_APP_STAY, headers=JSON_HEADER,
-                  data=json.dumps(app_stay_data), timeout=(10, 20))
+def create_remain_index():
+    requests.put(URL_ELASTICSEARCH_REMAIN)
+    requests.post(URL_ELASTICSEARCH_REMAIN + "/doc/_mapping",
+                  headers=JSON_HEADER, data=json.dumps(REMAIN_MAPPING), timeout=(10, 20))
+
+
+def set_remain_rate(key, rate, new_device_num):
+    remain_rate_key = "remain_rate_%dd" % key
+    dt = datetime.today() - timedelta(key+1)
+    _id = dt.strftime('%Y-%m-%d')
+    url = URL_ELASTICSEARCH_REMAIN_ID % _id
+    timestamp = dt.isoformat() + "+08:00"
+
+    r = requests.get(url, timeout=(10, 20))
+    r_json = r.json()
+    if r_json["found"]:
+        data = r_json["_source"]
+        data[remain_rate_key] = rate
+        requests.post(url, headers=JSON_HEADER,
+                      data=json.dumps(data), timeout=(10, 20))
+    else:
+        data = {
+            "@timestamp": timestamp,
+            remain_rate_key: rate,
+            "new_device_num": new_device_num
+        }
+        requests.post(url, headers=JSON_HEADER,
+                      data=json.dumps(data), timeout=(10, 20))
 
 
 if __name__ == '__main__':
+    # create_remain_index()
+    # set_remain_rate(5, 0.19, 68678)
     yud = uniq_device(URL_ELASTICSEARCH_APPLOG, get_query_device(1))
     nd1day = uniq_device(URL_ELASTICSEARCH_DEVICE, get_query_device(2))
     nd2day = uniq_device(URL_ELASTICSEARCH_DEVICE, get_query_device(3))
@@ -91,11 +149,13 @@ if __name__ == '__main__':
     nd5day = uniq_device(URL_ELASTICSEARCH_DEVICE, get_query_device(6))
     nd6day = uniq_device(URL_ELASTICSEARCH_DEVICE, get_query_device(7))
     nd7day = uniq_device(URL_ELASTICSEARCH_DEVICE, get_query_device(8))
-    nd15day = uniq_device(URL_ELASTICSEARCH_DEVICE, get_query_device(16))
+    nd14day = uniq_device(URL_ELASTICSEARCH_DEVICE, get_query_device(15))
     nd30day = uniq_device(URL_ELASTICSEARCH_DEVICE, get_query_device(31))
 
-    for nd in [nd1day, nd2day, nd3day, nd4day, nd5day, nd6day, nd7day, nd15day, nd30day]:
+    for key, nd in [(1, nd1day), (2, nd2day), (3, nd3day), (4, nd4day), (5, nd5day), (6, nd6day), (7, nd7day), (14, nd14day), (30, nd30day)]:
         ret = list(set(nd).intersection(set(yud)))
         if 0 == len(nd):
             continue
-        print len(ret)/float(len(nd))
+        rate = len(ret)/float(len(nd))
+        print key, rate, len(nd)
+        set_remain_rate(key, rate, len(nd))
