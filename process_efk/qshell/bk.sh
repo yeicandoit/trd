1fb46ad311232d833b5c44448824f45819d2519c
yeicandoit <410342333@qq.com>
2018-06-06 22:03:42 +0800

    Enable to backup harf automatically

--- process_efk/qshell/bk.sh
+++ process_efk/qshell/bk.sh
@@ -3,6 +3,10 @@ curl -H "Content-Type:application/json" -XPUT http://127.0.0.1:9200/_snapshot/$a
 curl -H "Content-Type:application/json" -XPUT http://127.0.0.1:9200/_snapshot/$applog/snapshot?wait_for_completion=true -d "{\"indices\":\"$applog\"}"
 
 zip -r -q /var/taoredian_es_bk/$applog.zip /var/taoredian_es_bk/$applog 
+mkdir -p /var/taoredian_es_bk/zip_$applog
+mv /var/taoredian_es_bk/$applog.zip /var/taoredian_es_bk/zip_$applog
+rm -rf /var/taoredian_es_bk/$applog
+
 sed -i "s/replace/$applog/g" qupload.json
-./qshell qupload 1 qupload.json
+./qshell qupload 1 qupload.json > log/$applog.upload.log
 sed -i "s/$applog/replace/g" qupload.json
